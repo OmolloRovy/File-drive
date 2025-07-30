5b8434eba61a9f5e762f8f6611d86cb42df9c0d4
OmolloRovy <masimevanistelrooy@gmail.com>
2025-07-31 02:23:15 +0300

    files.ts

--- convex/_generated/files.ts
+++ convex/_generated/files.ts
@@ -2,13 +2,12 @@ import { mutation } from "./_generated/server"
 import { v } from 'convex/values';
 
 export const createFile = mutation({
-    args: {
-        name: v.string(),
-    },
-    async handler( ctx, args ) {
-       await ctx.db.insert('files', {
-            name: args.name,
-          
-        });
-    }
+  args: {
+    name: v.string(),
+  },
+  async handler(ctx, args) {
+    await ctx.db.insert('files', {
+      name: args.name,
+    });
+  }
 })
