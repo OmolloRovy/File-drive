9fe6e053a51afc49dfbde5e34022f01da068db8b
OmolloRovy <masimevanistelrooy@gmail.com>
2025-07-31 01:33:28 +0300

    feat: implement file creation mutation in Convex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { SignInButton,SignedIn, SignedOut,SignOutButton } from '@clerk/nextjs'
+import { SignInButton,SignedIn, SignedOut,SignOutButton, useSession } from '@clerk/nextjs'
 
 export default function Home() {
   const session = useSession()
@@ -8,14 +8,19 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <SignedIn>
-          <SignOutButton mode="model" />
+          <SignOutButton mode="modal" >
+            <Button className="bg-red-500 text-white px-4 py-2 rounded">Sign Out</Button>
+          </SignOutButton>
         </SignedIn>
           <SignedOut>
-          <SignedInButton/>
+          <SignInButton>
+            <Button className="bg-blue-500 text-white px-4 py-2 rounded">Sign In</Button>
+          </SignInButton>
           </SignedOut>
-        <Button className="bg-blue-500 text-white px-4 py-2 rounded">
+        <Button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => {}}>
           Upload File
-        </Button></main>
-          </div>
+        </Button>
+      </main>
+    </div>
   );
 }
